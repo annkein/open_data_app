32a93abf1723d86be020291c36797ff0db84b20e
Anni <anni.keinonen@tuni.fi>
2026-02-10 16:59:50 +0200

    Add error handling, small fixes in code and comments

--- api_client.py
+++ api_client.py
@@ -1,17 +1,17 @@
 from dotenv import load_dotenv
 import os
 import requests
-import pandas
+import pandas as pd
 
 # Load the variables from .env -file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-# Only one dataset is in use
+# Since only one dataset is in use, set the id of it as a constant
 DATASET_ID = 124
 BASE_URL = "https://data.fingrid.fi/api/datasets"
 
-def fetch_data(start_time: str, end_time: str):
+def fetch_data(start_time: str, end_time: str) -> pd.DataFrame:
     """
     Fetch data from Fingrid Open Data API.
 
@@ -19,6 +19,9 @@ def fetch_data(start_time: str, end_time: str):
         start_time (str): Start time in ISO 8601 format, e.g. "2026-02-09T00:00:00Z"
         end_time (str): End time in ISO 8601 format
     """
+
+    if not API_KEY:
+        raise RuntimeError("API_KEY is missing. Check your .env file.")
 
     # Set the url, headers, and parameters for the API request
     url = f"{BASE_URL}/{DATASET_ID}/data"
@@ -29,17 +32,22 @@ def fetch_data(start_time: str, end_time: str):
         "format": "json"
     }
 
-    response = requests.get(url, headers=headers, params=params)
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        raise RuntimeError(f"Network or API error: {e}")
 
-    # Check for errors in response
-    if response.status_code != 200:
-        raise Exception(f"API request failed with status code {response.status_code}: {response.text}")
-    
-    data = response.json()
+    json_data = response.json()
+
+    # Check if data exists (All data from Fingrid Open Data used in 
+    # this program is under the key word "data")
+    if "data" not in json_data or not json_data["data"]:
+        raise ValueError("No data returned for the given time range.")
 
     # Convert data into DataFrame
-    df = pandas.DataFrame(data["data"])
-    df['startTime'] = pandas.to_datetime(df['startTime'])
-    df['endTime'] = pandas.to_datetime(df['endTime'])
+    df = pd.DataFrame(json_data["data"])
+    df['startTime'] = pd.to_datetime(df['startTime'])
+    df['endTime'] = pd.to_datetime(df['endTime'])
 
     return df
